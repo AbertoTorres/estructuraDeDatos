5c1b086bbe1c277bc7d377150f7aacda83b02005
AbertoTorres <abetopamo@hotmail.com>
2020-05-26 21:38:24 -0500

    v2

--- js/pilaClase.js
+++ js/pilaClase.js
@@ -1,21 +1,17 @@
-class pila {
+class Pila {
     constructor() {
         this.pila = [];
     }
 
-    //Metodos
-
-    //Agrega un elemento al final de la pila
     push(elemento) {
             this.pila.push(elemento);
             return this.pila[this.pila.length - 1]
         }
-
     
     pop() {
             return this.pila.pop();
         }
-
+        
     peek() {
         return this.pila[this.pila.length - 1]
     }
@@ -24,8 +20,70 @@ class pila {
         return this.pila.length;
     }
 
-
     print() {
         return this.pila;
     }
+}
+
+const pila = new Pila();
+
+let dato = document.getElementById("push");
+
+let cuerpo = document.getElementById("cuerpo");
+
+let div_c = document.getElementsByTagName("div");
+
+let b_push = document.getElementById("b_push")
+let b_peek = document.getElementById("b_peek")
+let b_size = document.getElementById("b_size")
+let b_print = document.getElementById("b_print")
+
+function Push() {
+    b_push.disabled = true;
+    div = document.createElement("div");
+    div.innerHTML = pila.push(dato.value);
+    cuerpo.appendChild(div);
+    setTimeout(() => {
+            div.style.display = "none"
+            b_push.disabled = false;
+        }, 1500) 
+}
+
+function Pop() {
+    pila.pop();
+    cuerpo.removeChild(div_c[div_c.length - 1])
+}
+
+function Peek() {
+    b_peek.disabled = true;
+    let elemento = document.createElement("span");
+    elemento.innerHTML = pila.peek();
+    cuerpo.appendChild(elemento);
+    setTimeout(() => {
+        cuerpo.removeChild(elemento);
+        b_peek.disabled = false;
+    }, 2500)
+}
+
+function Size() {
+    b_size.disabled = true;
+    let elemento = document.createElement("span");
+    elemento.innerHTML = pila.size();
+    cuerpo.appendChild(elemento);
+    setTimeout(() => {
+        cuerpo.removeChild(elemento);
+        b_size.disabled = false;
+    }, 2500)
+}
+
+function Print() {
+    b_print.disable = true;
+    pila.print().forEach((element) => {
+        let articulo = document.createElement("article");
+        articulo.innerHTML = element;
+        cuerpo.appendChild(articulo);
+        setTimeout(() => {
+            cuerpo.removeChild(articulo);
+        }, 2500)
+    });
 }
